65f05b24b768a5bf4500203e03bd417afb475581
Victor Kneider <victorkneider@gmail.com>
2023-10-21 00:39:03 -0400

    fix: changed code to connect to mongodb from deployment

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,20 @@
 import app from "./app.js";
-import connection from "./database.js";
+import mongoose, { mongo } from "mongoose";
 
-connection.once('open', () => {
-    console.log('Mongodb Connection stablished');
-  });
-  
-connection.on('error', (err) => {
-    console.log('Mongodb connection error:', err);
-    process.exit();
-});
+const connectDB = async () => {
+    try {
+        const conn = mongoose.connect(process.env.DB!)
+        console.log('Mongodb Connection stablished');
+    } catch (error) {
+        console.log('Mongodb connection error:', error);
+        process.exit();
+    }
+}
 
-app.listen(app.get('port'));
+connectDB().then(() => {
+    console.log('Mongodb connected');
+    app.listen(app.get('port'));
+    console.log('Server on port', app.get('port'));
+}
+);
 
-app.get('/', (req, res) => {
-    res.send('Hello World');
-});
-
-
-console.log('Server on port', app.get('port'));
